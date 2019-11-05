b6dccd3f20af34e9b0b89263fba11bb9c659a46a
agomez14@ucol.mx <agomez14@ucol.mx>
2019-11-05 10:14:55 -0600

    ya

---
+++ WpfApp1/WpfApp1/Clase/Contactos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace WpfApp1.Clase
+{
+    class Contactos
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public override string ToString()
+        {
+            return $"{Nombre} - {Correo} - {Telefono}"; 
+        }
+    }
+}
