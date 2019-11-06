1035c4844bfa797cb2c34825cd539c067d12ff05
Alumno <Alumno@PC06>
2019-11-06 12:15:56 -0600

    abr

---
+++ WpfApp1/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Clase;
+using SQLite;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List<Contactos> contactos;
+        public Window1()
+        {
+            InitializeComponent();
+            contactos = new List<Contactos>();
+            LeerBaseDatos();
+        }
+        void LeerBaseDatos()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contactos>();
+                contactos = (conn.Table<Contactos>().ToList()).OrderBy(c => c.Nombre).ToList();
+            }
+            if (contactos != null)
+            {
+                lvContactos.ItemsSource = contactos;
+            }
+        }
+    }
+}
